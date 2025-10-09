e17c2be305b1f5e0f27f3ffa16fcafe32e91f254
STharshan <sivatharshan81@gmail.com>
2025-10-10 02:37:15 +0530

    a

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import TermsConditions from "./components/Term";
 import Home from "./pages/Home";
 import PrivacyPolicy from "./components/PrivacyPolicy";
 import ScrollToTop from "./components/ScrollToTop";
+import GDPRConsent from "./components/GDPRButton";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         <Route path="/privacy-policy" element={<PrivacyPolicy />} />
       </Routes>
       <Footer />
+      <GDPRConsent />
     </Router>
   );
 }
